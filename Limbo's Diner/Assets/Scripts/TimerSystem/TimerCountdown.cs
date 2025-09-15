9a2336ec672b36646a692aeb3d54f472e637f04d
farmerferguson <gemmaferguson691@gmail.com>
2025-09-15 21:50:13 +0200

    Coding
    Coded basic countdown timer
    Tested to see it works

--- Limbo's Diner/Assets/Scripts/TimerSystem/TimerCountdown.cs
+++ Limbo's Diner/Assets/Scripts/TimerSystem/TimerCountdown.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class TimerCountdown : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float timeRemaining;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeRemaining -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt (timeRemaining/60);
+        int seconds = Mathf.FloorToInt (timeRemaining % 60);
+
+        timerText.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+
     }
 }
