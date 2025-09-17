19d5fd42dfddeb13ee8e3dd3c46928a90015a42f
farmerferguson <gemmaferguson691@gmail.com>
2025-09-17 21:14:50 +0200

    Dialogue system Working

--- Limbo's Diner/Assets/Scripts/npcs/interactionNPC.cs
+++ Limbo's Diner/Assets/Scripts/npcs/interactionNPC.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 
 public class interactionNPC : MonoBehaviour
 {
+    [SerializeField] public TextMeshProUGUI textComponent;
+    public string[] dialogueLines;
+    public float textSpeed;
+
+    private int index;
 
 
     public void Interact()
@@ -13,5 +18,62 @@ public class interactionNPC : MonoBehaviour
         //TESTING THAT INTERACTION WORKS, DELETE LATER
         Debug.Log ("Interact");
     }
+
+    
+    void Start ()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == dialogueLines[index])
+            {
+                nextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = dialogueLines[index];
+
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in dialogueLines[index].ToCharArray())
+        {   
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void nextLine()
+    {
+        if (index < dialogueLines.Length - 1)
+        {
+            index ++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+
 
 }
